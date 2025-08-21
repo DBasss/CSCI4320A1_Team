cf91645d1aaec7b7e54b473f2c5f416a494448ba
Cody Srey <cody.srey@gmail.com>
2025-08-21 18:54:05 -0400

    String calc edit to rotate string

--- CS4320A1/CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person1.java
+++ CS4320A1/CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person1.java
@@ -30,8 +30,11 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		if(input == null || input.length() < 2){
+			return input; // No input rotates
+		}
+
+		return unput.substring(2) + input.substring(0,2);
 	}
 	
 	/**
